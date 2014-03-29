e032aedff4d39a4e3a0e1f026b6e759c382378e2
tiago <tiago@wbserver.clogos.com.br>
2014-03-29 16:21:53 -0300

    Corrigido Erro de Validator JSF

--- ROOT/src/br/inf/gentec/site/validator/EmailValidator.java
+++ ROOT/src/br/inf/gentec/site/validator/EmailValidator.java
@@ -2,17 +2,19 @@ package br.inf.gentec.site.validator;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-public class EmailValidator
-    implements Validator
+@FacesValidator("emailValidator")
+public class EmailValidator implements Validator
 {
 
-    private static final String PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    //private static final String PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
     private Pattern pattern;
     private Matcher matcher;
 
